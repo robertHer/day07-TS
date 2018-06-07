090f4ce1d3bfcdc26af9d862b0fe123286f93d4f
unknown <totolero769@gmail.com>
2018-06-07 11:40:48 -0700

    Classes recap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,32 @@
 import { Component } from '@angular/core';
 
+class CarBrand{
+  protected make:string;
+  constructor(make:string){
+this.make = make;
+  }
+}
+let ford = new CarBrand('Ford');
+console.log(ford)
+
+class Car extends CarBrand{
+model:string;
+year:number;
+
+constructor(make:string,model:string, year:number){
+super(make);
+this.model = model;
+this.year = year;
+}
+
+getDetails(){
+  return (this.model,this.make, this.year);
+}
+
+}
+let honda = new Car('honda','model', 1990);
+console.log(honda)
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
